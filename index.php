c31bce25e529df669ab6a65b51519463950752a2
Girino Vey! <girinovey@girino.org>
2014-08-08 18:29:22 +0000

    rephrased stats cause this is a lottery, not a ponzi

--- index.php
+++ index.php
@@ -91,13 +91,12 @@
 					$('#unpaid').html(parseFloat(data['unpaid']).toFixed(<?php echo $config['precision'] ?>) + ' <?php echo $config['val'] ?>');
 					$('#received').html(parseFloat(data['received']).toFixed(<?php echo $config['precision'] ?>) + ' <?php echo $config['val'] ?>');
 					
-					var tmp20140808 = "error";
-					if (data['actual']['tx'] === undefined) { // nothing 
-					} else {
-						tmp20140808 = data['actual']['tx'].substring(0,32);
+					var tmp20140808 = "Pot: " + (parseFloat(data['received']) - parseFloat(data['paid'])).toFixed(<?php echo $config['precision'] ?>);
+					if (!(data['actual']['tx'] === undefined)) { // nothing 
+						tmp20140808 = tmp20140808 + " (" + parseFloat(data['actual']['topay']).toFixed(<?php echo $config['precision'] ?>) + " needed to pay " + data['actual']['tx'].substring(0,32) + "...)";
 					}
 					
-					$('#collecting').html("Collecting " + (parseFloat(data['received']) - parseFloat(data['paid'])).toFixed(<?php echo $config['precision'] ?>) + " / " + parseFloat(data['actual']['topay']).toFixed(<?php echo $config['precision'] ?>) + " for " + tmp20140808 + "...");
+					$('#collecting').html(tmp20140808);
 					
 					$('#trans').html('');
 					for(var i in data['transactions'])
