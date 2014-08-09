32ecaf01313f0d37b6edac74a64feb5c1dc16393
girino <girinovey@girino.org>
2014-08-09 17:41:40 -0300

    update json

--- json.php
+++ json.php
@@ -1,11 +1,12 @@
 <?php 
+	include('constants.php');
 	include('config.php');
 	$json = array();
 	
 	if ($_GET['what'] == "all" || $_GET['what'] == "")
-		$query = mysql_query('SELECT * FROM `transactions` WHERE `amount` > 0 AND `state` < 3 ORDER BY id DESC, state DESC LIMIT 20;');
+		$query = mysql_query('SELECT * FROM `transactions` WHERE `amount` > 0 AND `state` < ' . STATE_SENTBACK_READY . ' ORDER BY id DESC, state DESC LIMIT 20;');
 	else
-		$query = mysql_query('SELECT * FROM `transactions` WHERE `tx` = "' . mysql_real_escape_string($_GET['what']) . '" AND `state` < 3 LIMIT 1;');
+		$query = mysql_query('SELECT * FROM `transactions` WHERE `tx` = "' . mysql_real_escape_string($_GET['what']) . '" AND `state` < ' . STATE_SENTBACK_READY . ' LIMIT 1;');
 
 	$transactions = array();
 	while($row = mysql_fetch_assoc($query)) 
@@ -17,30 +18,30 @@
 	
 	if (sizeof($transactions) == 1)
 	{
-		$query = mysql_query("SELECT COUNT(*) FROM `transactions` WHERE `amount` > 0 AND `state` = 0 AND `date` < " . $transactions[0]['timestamp'] . ";");
+		$query = mysql_query("SELECT COUNT(*) FROM `transactions` WHERE `amount` > 0 AND `state` = " . STATE_INITIAL . " AND `date` < " . $transactions[0]['timestamp'] . ";");
 		$transactions[0]['queue'] = mysql_fetch_row($query)[0];
 	}
 	
 	$json['transactions'] = $transactions;
 	
 	// Actual transaction -- should be "current", since actual means something else
-	$query = mysql_query('SELECT * FROM `transactions` WHERE `amount` > 0 AND `state` = 0 ORDER BY date ASC LIMIT 1;');
+	$query = mysql_query('SELECT * FROM `transactions` WHERE `amount` > 0 AND `state` = ' . STATE_INITIAL . ' ORDER BY date ASC LIMIT 1;');
 	$json['actual'] = mysql_fetch_assoc($query);
 	
 	//$query = mysql_query("SELECT COUNT(col) FROM table");
-	$query = mysql_query("SELECT COUNT(*) FROM `transactions` WHERE `amount` > 0 AND `state` < 3;");
+	$query = mysql_query("SELECT COUNT(*) FROM `transactions` WHERE `amount` > 0 AND `state` < " . STATE_SENTBACK_READY . ";");
 	$query = mysql_fetch_row($query);
 	
 	$json['count'] = $query[0];
 	
 	// Received
-	$query = mysql_query("SELECT SUM(amount) FROM `transactions` WHERE `state` < 3;");
+	$query = mysql_query("SELECT SUM(amount) FROM `transactions` WHERE `state` < " . STATE_SENTBACK_READY . ";");
 	$query = mysql_fetch_row($query);
 	
 	$json['received'] = $query[0];
 	
 	// Unpaid
-	$query = mysql_query("SELECT SUM(topay) FROM `transactions` WHERE `state` = 0;");
+	$query = mysql_query("SELECT SUM(topay) FROM `transactions` WHERE `state` = " . STATE_INITIAL . ";");
 	$query = mysql_fetch_row($query);
 
 	if ($query[0] == null)
@@ -49,7 +50,7 @@
 		$json['unpaid'] = $query[0];
 	
 	// Paid
-	$query = mysql_query("SELECT SUM(topay) FROM `transactions` WHERE `state` > 0 AND `state` < 3;");
+	$query = mysql_query("SELECT SUM(topay) FROM `transactions` WHERE `state` > " . STATE_INITIAL . " AND `state` < " . STATE_SENTBACK_READY . ";");
 	$query = mysql_fetch_row($query);
 	$json['paid'] = $query[0];
 
