40314a76f31215b51965287ed25a6debea528e97
girino <girinovey@girino.org>
2014-08-10 00:31:01 -0300

    added state for retrieving money.

--- constants.php
+++ constants.php
@@ -3,9 +3,10 @@ define('STATE_INITIAL', 0);
 define('STATE_READY', 1);
 define('STATE_PAID', 2);
 define('STATE_POT_REFILL', 3);
-define('STATE_SENTBACK_READY', 4);
-define('STATE_SENTBACK', 5);
-define('STATE_ARCHIVED', 6);
+define('STATE_POT_RETRIEVAL', 4);
+define('STATE_SENTBACK_READY', 5);
+define('STATE_SENTBACK', 6);
+define('STATE_ARCHIVED', 7);
 
 define('COIN_DECIMAL_PLACES', 8);
 
