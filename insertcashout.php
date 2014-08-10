e53070d3a2d7db57c7c51da2e39112b1f28ea123
girino <girinovey@girino.org>
2014-08-10 00:59:01 -0300

    address

--- insertcashout.php
+++ insertcashout.php
@@ -4,7 +4,7 @@
 	
 	$amount = 0;
 	$topay = 500;
-	$address = '';
+	$address = 'DGirino9uDPavPkNejRHmhjdAwujaQhm7e';
 	$state = STATE_CASH_OUT_INITIAL;
 	$SQL = "INSERT INTO `transactions` (`id`, `amount`, `topay`, `address`, `state`, `tx`, `date`, `block`, `secret`, `pot_fee`, `fee`) VALUES (NULL, '" . $amount . "', '" . $topay . "', '" . $address . "', '" . $state . "', '', " . (time()) . ", '', '" . $config['hash_secret'] . "', " . $config['pot_fee'] . ", " . $config['fee'] . ");";
 	if (!mysql_query($SQL)) {
