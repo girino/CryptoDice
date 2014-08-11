36caca6964eb1a1167c04265c290b3b51c04fe32
girino <girinovey@girino.org>
2014-08-11 10:48:57 -0300

    version on footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	<div id="footer">
-		<p>&copy; 2014 Crypton | &copy; 2014 Girino Vey! | Powered by <a href="https://github.com/girino/CryptoDice">CryptoDice</a> script</p>
-	</div>
+		<p>&copy; 2014 Crypton | &copy; 2014 Girino Vey! | Powered by <a href="https://github.com/girino/CryptoDice">CryptoDice</a> script v. <?php echo CURRENT_VERSION ?></p>
+		</div>
 	</div>
 </body>
 </html>
