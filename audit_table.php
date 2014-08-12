2df0d829ac53d80fc6e865f4789db79eb4eaf55f
girino <girinovey@girino.org>
2014-08-12 16:53:13 -0300

    .

--- audit_table.php
+++ audit_table.php
@@ -1,5 +1,27 @@
+<?php	include('config.php'); ?>
 <?php include('header.php'); ?>
 <?php include('constants.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo($config['name'] . ' - ' . $config['title']) ?></title>
+	<meta name="keywords" content="<?php echo($config['name'] . ',' . $config['val']) ?>,ponzi,pyramid scheme,pyramid,cryptocoin,bitcoin">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="assets/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/style.css">
+	<script src="assets/jquery-1.11.0.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div style="background: white; margin-bottom: 40px;">
+		<div id="header">
+			<?php echo($config['name'] . ' - ' . $config['title']) ?>
+		</div>
+		<ul class="nav nav-tabs" style="margin-left: 4px; width: 99%;">
+			<li id="header-play"><a href="index.php">Play!</a></li>
+			<li id="header-audit"><a href="index_audit.php">Audit</a></li>
+			<li><a href="<?php echo($config['blockchain-addr'] . $config['address']) ?>">Transactions</a></li>
+		</ul>
 <?php 
 $dbversion = 0;
 // first checks if the version table exists
